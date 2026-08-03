06d33e58c4599e084241df9dd18f98709412d228
CryptoWorldz <jayjayteamdev@outlook.com>
2026-08-04 07:27:57 +1000

    Expand Worldz production domain map

--- apps/cryptoworldz-web-core/config/worlds.js
+++ apps/cryptoworldz-web-core/config/worlds.js
@@ -4,17 +4,34 @@ window.CRYPTOWORLDZ_CONFIG = {
   basedBidBoardUrl: "https://www.based.bid/b/Charity.Based",
   domains: {
     "cryptoworldz.xyz": { slug: "cryptoworldz", mode: "markets" },
-    "impactbased.oneworldz.com": { slug: "impactbased", mode: "impact" },
+    "www.cryptoworldz.xyz": { slug: "cryptoworldz", mode: "markets" },
+
     "oneworldz.com": { slug: "oneworldz", mode: "mission" },
+    "www.oneworldz.com": { slug: "oneworldz", mode: "mission" },
+    "impact.oneworldz.com": { slug: "impactbased", mode: "impact" },
+    "impactbased.oneworldz.com": { slug: "impactbased", mode: "impact" },
+
     "solworldz.xyz": { slug: "solworldz", mode: "world" },
+    "www.solworldz.xyz": { slug: "solworldz", mode: "world" },
     "ethworldz.xyz": { slug: "ethworldz", mode: "world" },
+    "www.ethworldz.xyz": { slug: "ethworldz", mode: "world" },
     "baseworldz.xyz": { slug: "baseworldz", mode: "world" },
+    "www.baseworldz.xyz": { slug: "baseworldz", mode: "world" },
     "bnbworldz.xyz": { slug: "bnbworldz", mode: "world" },
+    "www.bnbworldz.xyz": { slug: "bnbworldz", mode: "world" },
     "xrpworldz.xyz": { slug: "xrpworldz", mode: "world" },
+    "www.xrpworldz.xyz": { slug: "xrpworldz", mode: "world" },
     "suiworldz.xyz": { slug: "suiworldz", mode: "world" },
+    "www.suiworldz.xyz": { slug: "suiworldz", mode: "world" },
     "hyperworldz.xyz": { slug: "hyperworldz", mode: "world" },
+    "www.hyperworldz.xyz": { slug: "hyperworldz", mode: "world" },
     "robinworldz.xyz": { slug: "robinworldz", mode: "world" },
-    "bitcoinworldz.xyz": { slug: "bitcoinworldz", mode: "world" },
-    "hodlerworldz.xyz": { slug: "hodlerworldz", mode: "portfolio" }
+    "www.robinworldz.xyz": { slug: "robinworldz", mode: "world" },
+    "bitcoinworldz.xyz": { slug: "bitworldz", mode: "world" },
+    "www.bitcoinworldz.xyz": { slug: "bitworldz", mode: "world" },
+    "bitworldz.xyz": { slug: "bitworldz", mode: "world" },
+    "www.bitworldz.xyz": { slug: "bitworldz", mode: "world" },
+    "hodlerworldz.xyz": { slug: "hodlerworldz", mode: "portfolio" },
+    "www.hodlerworldz.xyz": { slug: "hodlerworldz", mode: "portfolio" }
   }
 };
