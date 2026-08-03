b449c9f5cc1793bc734e34388bed6865b3935e67
CryptoWorldz <jayjayteamdev@outlook.com>
2026-08-04 03:23:03 +1000

    Add multi-domain World configuration

---
+++ apps/cryptoworldz-web-core/config/worlds.js
@@ -0,0 +1,20 @@
+window.CRYPTOWORLDZ_CONFIG = {
+  supabaseUrl: "https://hknymhhyqldtzmplzuzh.supabase.co",
+  supabasePublishableKey: "sb_publishable_3ognbqSCTAcAnLHOeKZp8A_IgriwUJV",
+  basedBidBoardUrl: "https://www.based.bid/b/Charity.Based",
+  domains: {
+    "cryptoworldz.xyz": { slug: "cryptoworldz", mode: "markets" },
+    "impactbased.oneworldz.com": { slug: "impactbased", mode: "impact" },
+    "oneworldz.com": { slug: "oneworldz", mode: "mission" },
+    "solworldz.xyz": { slug: "solworldz", mode: "world" },
+    "ethworldz.xyz": { slug: "ethworldz", mode: "world" },
+    "baseworldz.xyz": { slug: "baseworldz", mode: "world" },
+    "bnbworldz.xyz": { slug: "bnbworldz", mode: "world" },
+    "xrpworldz.xyz": { slug: "xrpworldz", mode: "world" },
+    "suiworldz.xyz": { slug: "suiworldz", mode: "world" },
+    "hyperworldz.xyz": { slug: "hyperworldz", mode: "world" },
+    "robinworldz.xyz": { slug: "robinworldz", mode: "world" },
+    "bitcoinworldz.xyz": { slug: "bitcoinworldz", mode: "world" },
+    "hodlerworldz.xyz": { slug: "hodlerworldz", mode: "portfolio" }
+  }
+};
