14167ee0e7da820c7f8de20b22415bece5168b9c
CryptoWorldz <jayjayteamdev@outlook.com>
2026-09-18 23:42:12 +1000

    Fix audit regex quoting

--- tools/full_site_audit_core.py
+++ tools/full_site_audit_core.py
@@ -128,7 +128,7 @@ def static_audit() -> None:
             if not head:
                 errors.append(f'MISSING HEAD {f.relative_to(ROOT)}')
             else:
-                styles = re.findall(r'<link\\b[^>]*rel=["\\']stylesheet["\\'][^>]*>', head.group(1), re.I)
+                styles = re.findall(r"<link\\b[^>]*rel=['\"]stylesheet['\"][^>]*>", head.group(1), re.I)
                 if not styles or '/global-image-overrides.css' not in styles[-1]:
                     errors.append(f'GLOBAL IMAGE STYLE NOT LAST {f.relative_to(ROOT)}')
         if 'data-oneworldz-build=' not in text:
@@ -351,7 +351,7 @@ def live_audit() -> None:
             head = re.search(r'<head\\b[^>]*>([\\s\\S]*?)</head>', text, re.I)
             if not head:
                 return f'LIVE PAGE HEAD MISSING {url}'
-            styles = re.findall(r'<link\\b[^>]*rel=["\\']stylesheet["\\'][^>]*>', head.group(1), re.I)
+            styles = re.findall(r"<link\\b[^>]*rel=['\"]stylesheet['\"][^>]*>", head.group(1), re.I)
             if not styles or '/global-image-overrides.css' not in styles[-1]:
                 return f'LIVE GLOBAL IMAGE STYLE NOT LAST {url}'
         return None
